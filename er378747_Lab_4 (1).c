6ed2504c2feaec49d8d3b73da37e778db99eb996
eamatt2 <125511922+eamatt2@users.noreply.github.com>
2023-02-16 21:34:14 -0500

    Add files via upload

---
+++ er378747_Lab_4 (1).c
@@ -0,0 +1,98 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    char letter;
+    struct node* next;
+} node;
+
+// Returns number of nodes in the linkedList.
+int length(node* head)
+{
+    int count = 0;
+    while(head!=NULL){
+        count++;    //traverses through the list
+        head = head -> next;
+    }
+    return count;
+        
+}
+
+// passes the string in the linkedList
+//  if the linked list is head -> |a|->|b|->|c|
+//  then toCString function wil return "abc"
+char* toCString(node* head)
+{
+    int len = length(head);
+    char* str = malloc(len + 1);//makes up for NULL character at end of string
+    int i = 0;
+    while(head != NULL){
+        str[i++] = head -> letter;
+        head = head -> next;
+    }
+    str[i] = '\0';
+    return str;
+}
+
+// inserts character to the linkedlist
+// f the linked list is head -> |a|->|b|->|c|
+// then insertChar(&head, 'x') will update the linked list as foolows:
+// head -> |a|->|b|->|c|->|x|
+void insertChar(node** pHead, char c)
+{
+    node* newNode = malloc(sizeof(node));//have to malloc for the new node that was created
+    newNode -> letter = c;
+    newNode -> next = *pHead;
+    *pHead = newNode;
+}
+
+// deletes all nodes in the linkedList.
+void deleteList(node** pHead)
+{
+    node* current = *pHead;
+    while(current){
+        node* next = current -> next;
+        free(current); //frees the node so no memory leak
+        current = next;
+    }
+    *pHead = NULL;
+}
+
+int main(void)
+{
+    int i, strLen, numInputs;
+    node* head = NULL;
+    char* str;
+    char c;
+    FILE* inFile = fopen("input.txt","r");
+    fscanf(inFile, " %d\n", &numInputs);
+    while (numInputs-- > 0)
+    {
+        fscanf(inFile, " %d\n", &strLen);
+        for (i = 0; i < strLen; i++)
+        {
+            fscanf(inFile," %c", &c);
+            insertChar(&head, c);
+        }
+        str = toCString(head);
+        printf("String is : %s\n", str);
+        
+        free(str);
+        deleteList(&head);
+        
+        if (head != NULL)
+        {
+            printf("deleteList is not correct!");
+            break;
+        }
+    }
+    fclose(inFile);
+}
